ca5cb64b8167478db6411893389b041d3bceba41
sarzi <85951383+StwFate@users.noreply.github.com>
2024-09-29 01:38:19 -0400

    Update QueueTest.lua

--- QueueTest.lua
+++ QueueTest.lua
@@ -1,5 +1,5 @@
 game.Players.LocalPlayer.OnTeleported:Connect(function()
-    queue_on_teleport("loadstring(game:HttpGet(''))()")
+    queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/StwFate/others/refs/heads/main/QueueTest.lua'))()")
 end)
 
 print("Hi")
